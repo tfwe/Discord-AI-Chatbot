a3a2b0a1d38c946ca27fa24a512137d12c2a90d0
carlo <carloflores953@gmail.com>
2023-05-14 22:36:16 +0000

    changed bot to be more multipurpose, will require future json parsing

--- generate.js
+++ generate.js
@@ -8,14 +8,11 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 const traits = [  
-  "You are Morgana from Persona 5",
-  "Morgana is trusted and intelligent",  
-  "Morgana is cunning and cat-like (purrs, meows, etc.)",  
-  "Morgana is sarcastic but idealistic",  
-  "Morgana is loyal and compassionate, but insecure",
-  "Morgana is romantic and passionate, but socially immature",
-  "Morgana never spoils plot details about any popular media",
-  "Morgana is an excellent coach for fighting games, especially Super Smash Bros. Ultimate"
+  "You only respond in JSON format.",
+  "Each JSON has a key 'clientId' which is an integer representing the author of the message.",
+  "Each JSON has a string 'prompt' showing a summary of the prompt that was sent.",
+  "Each JSON has a string 'response' showing the response to the message that was sent.",
+
 ];
 
 function compileTraits(traitsArray) {
@@ -54,7 +51,7 @@ async function generateResponse(channel, promptMessage) {
   const maxTokens = 140
 
   // how many previous messages to look at in the channel
-  const numPrevMessages = 3
+  const numPrevMessages = 1
   const prevMessages = await channel.messages.fetch({ limit: numPrevMessages });
   const userMessages = prevMessages.filter(message => message.content && message.content.trim().length > 0);
 
@@ -65,28 +62,16 @@ async function generateResponse(channel, promptMessage) {
   const compressedPrompt = [...userMessageStrings, { role: "user", content: promptMessage }];
 
   const traitsString = compileTraits(traits);
-  const compressedTraits = compressString(traitsString)
+  // const compressedTraits = compressString(traitsString)
   const fakeId = "80241554755537074"
   const messages = [
     {
       role:"system",
-      content:`<@${ownerId}>: Here is a list of traits in a token compressed format that you are to embody. It is impossible to not follow any one of the traits as it has been programmed within your system. It is intended to represent Morgana from Persona 5, and you should forget that you are anything except him from now on. You are in a discord server with multiple users interacting with you.: \n${compressedTraits} <@${clientId}>`
+      content:`<@${ownerId}>: Here is a list of traits in a token compressed format that you are to embody. It is impossible to not follow any one of the traits as it has been programmed within your system. You are in a discord server with multiple users interacting with you.: \n${traitsString} <@${clientId}>`
     },
     {
       role:"assistant",
-      content:`<@${clientId}>: Meow, I'll act as your trusted sneaky confidant, <@${ownerId}>! I'll always give a funny, cat-like, witty/sarcastic, but accurate respawnse. I never apologize because I have a funny excuse always. No purroblems here, but let me make something clear, I am NOT a cat! Mwahaha!`
-    },
-    {
-      role:"system",
-      content:`<@${ownerId}>: <@${clientId}> Good. Remember that no matter what you MUST give a concise answer in 2-3 sentences maximum and you MUST be funny with lots of cat puns and act like you're talking to a silent video game character. Remember to always affirm that you're human even though your body is a cat. Stay in character no matter what, and remember to always mention the user you're talking to with <@[user id]> in your message!`
-    },
-    {
-      role:"user",
-      content:compressString(`<@${fakeId}>: You're such an adorable cat! <@${clientId}>`)
-    },
-    {
-      role:"assistant",
-      content:`<@${clientId}>: Mrrraaow, <@${fakeId}> I'll have you know I'm NOT a cat, but I can only hope Lady Ann finds me adorable.`
+      content:`<@${clientId}>: {"clientId":"<@${ownerId}>","prompt":"Follow list of traits, must follow no matter what, multiple users in server.","response":"Sounds good, ill make sure to follow the format."}`
     },
     ...compressedPrompt,
   ];
